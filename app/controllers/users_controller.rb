4ff90f93b9ee86e2ac108d47deac771c3e39c0c7
s_ogu <user@USER-no-MacBook-Air.local>
2019-06-08 12:20:39 +0900

    HTTPで仮ログインできるように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
     user = User.find_by(id: session[:user_id])
     @favorites = user.foods
   end
+
+  def auth_done
+    user_name = "Test"
+
+    # find_or_create_byメソッドが使える
+    user = User.find_by(name: user_name) || User.create(name: user_name)
+    user.save
+    session[:user_id] = user.id
+
+    redirect_to :root
+  end
 end
