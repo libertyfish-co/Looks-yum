8bbcce82e8c54133f20120c6fe350a096a1df004
soruma <soruma.net@gmail.com>
2019-08-03 17:01:01 +0900

    Fix Layout/SpaceInsideBlockBraces

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -26,7 +26,7 @@ class FoodsController < ApplicationController
     end
 
     submit_point_id = []
-    params[:point].map { |id| submit_point_id << id[:id].to_i}
+    params[:point].map { |id| submit_point_id << id[:id].to_i }
     if @food.save
       # pointテーブルへ登録
       submit_point_id.each do |point_id|
